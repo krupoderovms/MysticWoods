3c9f679fbfb6d1db2e38cdafbeb0b5d83b233a0f
KrupoderovMikhail <krupoderov.mikhail@gmail.com>
2022-11-04 22:09:00 +0300

    Initial commit.

---
+++ core/src/main/kotlin/com/github/krupoderovms/mysticwoods/MysticWoods.kt
@@ -0,0 +1,6 @@
+package com.github.krupoderovms.mysticwoods
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class MysticWoods : ApplicationAdapter()
