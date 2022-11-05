5a94734a57520da2caaeff93f3f7ae6beb92a24b
KrupoderovMikhail <krupoderov.mikhail@gmail.com>
2022-11-05 11:29:34 +0300

    Add screen.

--- core/src/main/kotlin/com/github/krupoderovms/mysticwoods/MysticWoods.kt
+++ core/src/main/kotlin/com/github/krupoderovms/mysticwoods/MysticWoods.kt
@@ -1,6 +1,16 @@
 package com.github.krupoderovms.mysticwoods
 
-import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Application
+import com.badlogic.gdx.Gdx
+import com.github.krupoderovms.mysticwoods.screen.GameScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class MysticWoods : ApplicationAdapter()
+class MysticWoods : KtxGame<KtxScreen>() {
+
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+        addScreen(GameScreen())
+        setScreen<GameScreen>()
+    }
+}
