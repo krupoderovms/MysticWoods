eace63577f5f2a7037612917a2b55bc86a10b6f4
KrupoderovMikhail <krupoderov.mikhail@gmail.com>
2022-11-05 12:14:12 +0300

    Add entity component system.

--- core/src/main/kotlin/com/github/krupoderovms/mysticwoods/screen/GameScreen.kt
+++ core/src/main/kotlin/com/github/krupoderovms/mysticwoods/screen/GameScreen.kt
@@ -3,8 +3,11 @@ package com.github.krupoderovms.mysticwoods.screen
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.viewport.ExtendViewport
+import com.github.krupoderovms.mysticwoods.component.ImageComponent
+import com.github.krupoderovms.mysticwoods.component.ImageComponent.Companion.ImageComponentListener
+import com.github.krupoderovms.mysticwoods.system.RenderSystem
+import com.github.quillraven.fleks.World
 import ktx.app.KtxScreen
 import ktx.assets.disposeSafely
 import ktx.log.logger
@@ -13,16 +16,24 @@ class GameScreen : KtxScreen {
 
     private val stage : Stage = Stage(ExtendViewport(16f, 9f))
     private val texture : Texture = Texture("assets/graphics/player.png")
+    private val world : World = World {
+        inject(stage)
+
+        componentListener<ImageComponentListener>()
+
+        system<RenderSystem>()
+    }
 
     override fun show() {
         log.debug { "GameScreen gets show" }
-        stage.addActor(
-            Image(texture).apply {
-                setPosition(1f, 1f)
-                setSize(1f, 1f)
-                setScaling(Scaling.fill)
+
+        world.entity {
+            add<ImageComponent> {
+                image = Image(texture).apply {
+                    setSize(4f, 4f)
+                }
             }
-        )
+        }
     }
 
     override fun resize(width: Int, height: Int) {
@@ -30,15 +41,13 @@ class GameScreen : KtxScreen {
     }
 
     override fun render(delta: Float) {
-        with(stage) {
-            act(delta)
-            draw()
-        }
+        world.update(delta)
     }
 
     override fun dispose() {
         stage.disposeSafely()
         texture.disposeSafely()
+        world.dispose()
     }
 
     companion object {
